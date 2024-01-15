4476a14c6fb920324c02a08b85d0470a421dc38c
Felipe Tulio <eng.felipetulio@gmail.com>
2024-01-15 16:05:58 -0300

    Adicionando comentários explicativos no arquivo app.js

--- src/app.js
+++ src/app.js
@@ -11,6 +11,8 @@ const app = express();
 // de dados em json, precisamos desse middleware para "parsear" antes de usarmos.
 app.use(express.json())
 
+// este é o array de objetos que está sendo utilizado como "armazenamento dos dados"
+// inseridos e buscados na API.
 const livros = [
     {
         id: 1,
@@ -26,7 +28,8 @@ const livros = [
     }
 ]
 
-
+// função criada para retornar o índice de um objeto no array livros[] com base no id
+// passado pela requisição.
 function buscaLivro(id) {
     return livros.findIndex(livro => {
         return livro.id === Number(id);
@@ -50,23 +53,35 @@ app.get("/livros/:id", (req, res) => {
 })
 
 
-// o método .post() tem uma tarefa contrária ao do método .get(). Ao invés de 
+// o método .post() tem uma tarefa contrária ao do método .get(). Ao invés de "capturar"
+// dados do sistema e retorná-los para o usuário, o método .post() inclui dados na API,
+// que eventualmente serão armazenados em um banco de dados. Neste exemplo, os dados inseridos
+// serão incluídos no array de objetos livros[].
 app.post("/livros", (req, res) => {
     livros.push(req.body);
     res.status(201).send("livro cadastrado com sucesso");
 });
 
+
+// o método .put() faz uma atualização do conteúdo. O método HTTP PUT geralmente é utilizado para
+// atualizar ou criar um recurso no servidor, ou seja, é utilizado quando deseja-se modificar um
+// recurso específico no servidor ou criar um novo se ele não existir.
 app.put("/livros/:id", (req, res) => {
     const index = buscaLivro(req.params.id);
     livros[index].titulo = req.body.titulo;
     res.status(200).json(livros);
 });
 
+
+// o método .delete() é utilizado em APIs JavaScript para solicitar a remoção de um recurso
+// específico no servidor. Quando você faz uma requisição DELETE, está indicando ao servidor que
+// deseja excluir o recurso identificado pela URL da requisição.
 app.delete("/livros/:id", (req, res) => {
     const index = buscaLivro(req.params.id);
     livros.splice(index, 1);
     res.status(200).send("livro removido com sucesso");
 });
+
 
 // exportação de variáveis e métodos do arquivo app.js para os demais arquivos do projeto.
 // Aqui o arquivo app.js se torna um módulo dentro do projeto.
